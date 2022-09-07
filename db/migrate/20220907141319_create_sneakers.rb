948999ce80866a0ff8f0d034f298afbc037d4a9e
Bryan Beshore <bryan.beshore@gmail.com>
2022-09-07 10:16:00 -0400

    Added sneaker scaffold + active storage with image attachment for sneakers

---
+++ db/migrate/20220907141319_create_sneakers.rb
@@ -0,0 +1,13 @@
+class CreateSneakers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sneakers do |t|
+      t.string :name
+      t.string :brand
+      t.string :string
+      t.text :description
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
